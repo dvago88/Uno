c306bf8323915c99043a24c300d00c3a6f2d8381
Daniel Vargas <dvago1988@gmail.com>
2017-03-12 18:34:26 -0500

    Lista la repartición de cartas al computador segun la cantidad de rivales

--- src/Game.java
+++ src/Game.java
@@ -1,10 +1,16 @@
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Game {
     private LinkedList<String> mDequeOfCards = new LinkedList<>();
+    private LinkedList<String> mFirstRival = new LinkedList<>();
+    private LinkedList<String> mSecondRival = new LinkedList<>();
+    private LinkedList<String> mThirdRival = new LinkedList<>();
     private String[] mColores = {"R", "M", "V", "A", "R", "M", "V", "A"};
     private Prompter prompter;
+    private Iterator<String> iterator;
+
 
     public Game() {
         fillCards();
@@ -24,11 +30,30 @@ public class Game {
         }
     }
 
-    public void shuffleCards(LinkedList<String> list){
+    public void shuffleCards(LinkedList<String> list) {
         Collections.shuffle(list);
     }
 
-    public void dealTheCards(){
+    public void dealTheCards() {
+        int numberOfRivals = prompter.getNumberOfPlayers();
+        switch (numberOfRivals) {
+            case 1:
+                iterator=mDequeOfCards.listIterator();
+                for(int i=0;i<7;i++){
+                    mFirstRival.add(iterator.next());
+                }
+                break;
+            case 2:
+                for(int i=0;i<7;i++){
+                    mSecondRival.add(iterator.next());
+                }
+                break;
+            case 3:
+                for(int i=0;i<7;i++){
+                    mThirdRival.add(iterator.next());
+                }
+                break;
+        }
 
     }
 
