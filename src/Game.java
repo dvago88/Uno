c43c4a28b679ab5ec55b0a83db2d4bf33342f661
Daniel Vargas <dvago1988@gmail.com>
2017-03-11 23:55:38 -0500

    Se llena la matriz de las cartas sin tener en cuenta el color

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,28 @@
 
 public class Game {
-    
+    private int[][] mCards =new int[8][9];
+
+    public Game() {
+
+    }
+
+    public int[][] getCards() {
+        return mCards;
+    }
+
+    public void setCards(int[][] cards) {
+        mCards = cards;
+    }
+
+    public void fillCards(){
+        for(int i=0;i<8;i++){
+            int counter=0;
+            for(int j=0;j<9;j++){
+                mCards[i][j]=counter;
+                counter++;
+            }
+        }
+    }
+
+
 }
