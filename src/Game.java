7128b0a7933cb8e184eab1e11041c7709bb6be22
Daniel Vargas <dvago1988@gmail.com>
2017-03-12 04:46:47 -0500

    Cambio de matrz or una lista

--- src/Game.java
+++ src/Game.java
@@ -1,28 +1,29 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Game {
-    private String[][] mCards =new String[8][9];
-    private String [] mColores={"R","M","V","A","R","M","V","A"};
+    private LinkedList<String> mDequeOfCards = new LinkedList<>();
+    private String[] mColores = {"R", "M", "V", "A", "R", "M", "V", "A"};
+
+
 
     public Game() {
         fillCards();
+        shuffleCards(mDequeOfCards);
     }
 
-    public String[][] getCards() {
-        return mCards;
-    }
-
-    public void setCards(String[][] cards) {
-        mCards = cards;
-    }
-
-    public void fillCards(){
-        for(int i=0;i<8;i++){
-            int counter=1;
-            for(int j=0;j<9;j++){
-                mCards[i][j]=counter+mColores[i];
-                counter++;
+    public void fillCards() {
+        for (int i = 0; i < 8; i++) {
+            int counter = 1;
+            for (int j = 0; j < 9; j++) {
+                mDequeOfCards.add(counter + mColores[i]);
             }
         }
+    }
+
+    public void shuffleCards(LinkedList<String> list){
+        Collections.shuffle(list);
     }
 
 
--- src/Jugador.java
+++ src/Jugador.java
@@ -1,5 +1,4 @@
-/**
- * Created by daniel on 12/03/17.
- */
+
 public class Jugador {
+
 }
