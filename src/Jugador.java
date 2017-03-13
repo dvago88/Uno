b803985572f0646781314a3bfdb94d9499f4a577
Daniel Vargas <dvago1988@gmail.com>
2017-03-13 04:28:26 -0500

    agregadr TODO a jugador

--- src/Jugador.java
+++ src/Jugador.java
@@ -2,6 +2,7 @@ import java.util.LinkedList;
 
 public class Jugador {
     private LinkedList<String> mMyCards;
+    //TODO agregar metodo que lea el nombre del jugador en el prompter
     private String mName;
 
     public Jugador() {
