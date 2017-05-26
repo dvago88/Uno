6d1d3285de8a20ac974736740b065fd9d0088a5d
Juan Sebastian Gómez Giraldo <sebastiangudea@gmail.com>
2017-05-26 01:23:37 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
         Jugador jugador = new Jugador();
         Game game = new Game(jugador);
-        Prompter prompter = new Prompter(jugador, game);
-        prompter.gamePresentation();
+        Prompter.gamePresentation();
 
         game.howManyPlayers();
         game.fillCards();
         game.dealTheCards();
 
         while (!game.isGameOver()) {
-            prompter.showPlayedCard();
-            prompter.showPlayerCards();
             game.play();
 
         }
